483bcd506a38ae8a3bbe71ad47698e26e1863a47
surunzi <670785827@qq.com>
2015-10-14 21:59:40 +0800

    Add: FindKey

---
+++ src/findKey.js
@@ -0,0 +1,14 @@
+'cb keys';
+
+exports = function (obj, predicate, ctx)
+{
+    predicate = cb(predicate, ctx);
+
+    var _keys = keys(obj), key;
+
+    for (var i = 0, len = _keys.length; i < len; i++)
+    {
+        key = _keys[i];
+        if (predicate(obj[key], key, obj)) return key;
+    }
+};
