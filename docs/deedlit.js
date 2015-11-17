3a658f67bbca36e53c1f011b2b38911896cef57d
surunzi <surunzi@foxmail.com>
2015-11-17 22:15:04 +0800

    Del: Docs

--- docs/deedlit.js
+++
@@ -1,4 +0,0 @@
-var deedlit = require('deedlit');
-
-deedlit.src('').task('').dest().start();
-deedlit.src('').options({}).task().dest().start();
--- docs/docs/index.js
+++
@@ -1,1 +0,0 @@
-exports.class = require('./class');
