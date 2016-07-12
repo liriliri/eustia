b9dd43942fdb4f43e9e589dd06bc0d303b81bec4
surunzi <surunzi@foxmail.com>
2016-07-12 15:52:56 +0800

    Add: Build test

--- test/build.js
+++ test/build.js
@@ -1,15 +1,36 @@
+var chai = require('chai'),
+    fs = require('fs'),
+    path = require('path');
+
 var eustia = require('../index');
+
+var expect = chai.expect;
 
 describe('build', function ()
 {
-    it('should has trim included', function (done)
+    it('basic', function (done)
     {
         eustia.build({
             files: './test/build/index.html',
-            output: './test/build/util.js'
+            output: './test/build/util.basic.js'
         }, function (err)
         {
-            done(err);
+            if (err) done(err);
+
+            readFile('./build/util.basic.js', function (err, data)
+            {
+                if (err) done(err);
+
+                expect(data).to.contain('has');
+                expect(data).to.contain('now');
+
+                done();
+            });
         });
     });
 });
+
+function readFile(filePath, cb)
+{
+    fs.readFile(path.resolve(__dirname, filePath), 'utf-8', cb);
+}
